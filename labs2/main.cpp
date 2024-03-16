8e21fe019472d8dd8a1da8784dc7b0e8139897a1
aanastasiaakk <klyvchuka@gmail.com>
2024-03-16 16:27:01 +0200

    Demonstrate polymorphism

--- labs2/main.cpp
+++ labs2/main.cpp
@@ -7,73 +7,15 @@
 using namespace std;
 
 int main() {
-    Vehicle v;
-    Parkingticket p;
-    User u;
-    User s;
-    User q;
-    Car w;
-    Car z;
-    Truck o;
-
-    cout<<"Enter information about user q:" << endl;
-    cin >> q;
-
-    cout << endl;
-
-    cout << "My car: " << endl;
-    cin >> w;
-
-    cout << endl;
-
-    cout << "My truck: " << endl;
-    cin >> o;
-
-    cout << endl;
-    v.setBrand("BMW");
-    v.setPlateNumber("CE0242CO");
-    p.setDate(22032024);
-    p.setNumberTicket(123456);
-    u.setAge(19);
-    u.setName("Sophia");
-    s.setName("Angie");
-    s.setAge(20);
-    z.setNumberOfDoors(3);
-    w.setNumberOfDoors(5);
-
-    cout << "Compare car z and car w" << endl;
-
-    if (z.getNumberOfDoors() == w.getNumberOfDoors()){
-        cout << "They have the same number of doors" << endl;
-    } else {
-        cout << "They have a different number of doors" << endl;
-    }
-
-    cout << endl;
-
-    Vehicle copyVehicle = v;
-    Vehicle moveVehicle = move(copyVehicle);
-
-    v.printInfo();
-    p.printInfo();
-    u.printInfo();
-    s.printInfo();
-    q.printInfo();
-    copyVehicle.printInfo();
-    moveVehicle.printInfo();
-    w.printInfo();
-    o.printInfo();
-
-    cout << endl;
-
-    if (s.getAge() == u.getAge()) {
-        cout << "Angie and Sophia are the same age.\n";
-    } else {
-        cout << "Angie and Sophie are different ages.\n";
-    }
-
-    cout << endl;
-
+    Vehicle* v = new Car("BMW","CE0242",4);
+    v->printInfo();
+    Vehicle* t = new Truck("MAN","AT4536CE",22);
+    t->printInfo();
+    Car car("AUDI","CE2345CV",4);
+    Vehicle& carRef = car;
+    car.startEngine();
+    delete v;
+    delete t;
 
 
     return 0;
